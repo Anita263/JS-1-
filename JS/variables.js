5fdb98074ba30420b401517abf188eade7a862f5
Анна <112005579+Anita263@users.noreply.github.com>
2022-11-02 17:37:59 +0800

    Add files via upload

---
+++ JS/variables.js
@@ -0,0 +1,16 @@
+let myAge = '33';
+let myBestFriendAge = '32';
+let yearOfMyBirth = '1989';
+let currentYear = '2022';
+let rublesInDollar = '61,88';
+
+
+
+const myName = 'Anna';
+const  myBestFriendName = 'Veronica';
+const myCity = 'Kaliningrad';
+const capitalOfMyCountry = 'Moskva';
+const oneWordDescriptionOfMe = 'scorpion';
+
+
+console.log(`Меня зовут ${myName}, мне ${myAge} года.Я из города ${myCity} и я ${oneWordDescriptionOfMe}.`);
